8e33819b39f6b5ade30ad4dc2e1d28c3b6031775
ws903 <ws903@nyu.edu>
2018-11-21 16:37:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const app = "I don't do much.";
+const token = "1dd70fa59952c97a545b68f96b3adfb409e46b1a"
+
+fetch('https://api.github.com/ws903/repos', {
+	headers: {
+		Authorization: `token ${token}`
+	}
+}).then(res => res.json()).then(json => console.log(json))
